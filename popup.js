d8961913e8c8a44c8f99413fc5a01b3ef5cf1295
linzb <lin302010@qq.com>
2021-08-11 13:35:22 +0800

    个人插件init

---
+++ popup.js
@@ -0,0 +1,14 @@
+﻿console.log("你好，我是popup！");
+let url = "";
+chrome.tabs.getSelected(null, function (tab) {
+  //获取 当前标签页的url
+  url = tab.url;
+  // 孕育：
+  appurl = `https://app.mama.cn/soft_link/pt?type=666&url=${encodeURIComponent(
+    url
+  )}`;
+});
+$(function () {
+  new QRCode(document.getElementById("qrcode"), appurl); // 设置要生成二维码的链接
+  $("#taburl").html("当前页面url:" + url);
+});
