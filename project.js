3fea050624f916ef0bf631c3b574bc4b278ab370
linzb <lin302010@qq.com>
2021-09-01 12:14:36 +0800

    新增项目

--- project.js
+++ project.js
@@ -23,6 +23,10 @@ const project = {
   ],
   bbmamacn: [
     {
+      name: "邀亲亲友-接受邀亲页",
+      url: "http://bb.mama.cn/wap/yqhuodong?link_code=64DD6AF0F29EECC3DF066E3B193E9535&tkey=1629082126&uni_code=41b8c5e2&uid=106537132&bid=647446&babyname=%E6%9B%BE%E7%BB%8F%E8%AE%A4%E5%8F%AF&relation_id=23&username=%E6%88%91%E6%98%AF%E6%9C%A8%E9%B1%BC&source=5#/sharec",
+    },
+    {
       name: "邀亲亲友",
       url: "http://bb.mama.cn/wap/yqhuodong#/",
     },
